3041361e618b9267bf3431efef1f73f2de5cb31c
JuanCamilo268 <juleonba@unal.edu.co>
2025-11-19 16:26:52 -0500

    Update analisis.py

--- analisis.py
+++ analisis.py
@@ -42,6 +42,8 @@ def diferencial(V1: tuple, V2:tuple):
     recien = V1[1]
     return (V1[0], recien - lejos)
 
+def aumento(valor:float, recargo:float): return valor + valor * recargo / 100
+
 def recomendacion(producto: str):
     tabla = Path(__file__).with_name("contabilidad_lechera.db")
 
@@ -97,9 +99,12 @@ def recomendacion(producto: str):
         if menos_creciente[1] == "None" or dif[1] < menos_creciente[1]:
             menos_creciente = dif
 
-    return ("*La empresa más económica es:\n{} con ${}".format(mas_barata[0], mas_barata[1]),
-        "*La empresa más confiable es:\n{} con {} llamados".format(mas_llamada[0], mas_llamada[1]),
-        "*La empresa con menos aumento\nde precios es:\n{} con ${}".format(menos_creciente[0], menos_creciente[1]))
+    return ("*La empresa más económica es:\n" +
+            "{} con ${}".format(mas_barata[0], mas_barata[1]),
+        "*La empresa más confiable es:\n" +
+        "{} con {} llamados".format(mas_llamada[0], mas_llamada[1]),
+        "*La empresa con menos aumento\n" +
+        "de precios es:\n{} con ${}".format(menos_creciente[0], menos_creciente[1]))
 
 def general():
     tabla = Path(__file__).with_name("contabilidad_lechera.db")
@@ -109,9 +114,35 @@ def general():
 
     cursor.execute("SELECT * FROM facturas")
     filas = cursor.fetchall()
+    en_tres = {}
+    en_uno = {}
+    for fila in filas:
+        if fila[1] in en_tres:
+            en_tres[fila[1]] += aumento(fila[6] * fila[4], fila[7])
+        elif hace_tres_meses(fila[2]):
+            en_tres[fila[1]] = aumento(fila[6] * fila[4], fila[7])
+    
+    for fila in filas:
+        if fila[1] in en_uno:
+            en_uno[fila[1]] += aumento(fila[6] * fila[4], fila[7])
+        elif hace_un_mes(fila[2]):
+            en_uno[fila[1]] = aumento(fila[6] * fila[4], fila[7])
+
+    if len(en_tres) < 1: return (-1, -1, -1)
+    promedio = 0
+    mayor = ("", 0)
+    for elemento in en_tres:
+        promedio += en_tres[elemento]
+        if en_tres[elemento] > mayor[1]:
+            mayor = (elemento, en_tres[elemento])
+    dif = sum(en_tres.values()) - sum(en_uno.values())
+    promedio /= 3
+    return ("*En los últimos tres meses\nse gastó en promedio un total de\n${}".format(dif),
+            "*En los últimos tres meses el\nproducto en el que más se ha\ninvertido es {} con un\ntotal de ${}".format(mayor[0], mayor[1]),
+            "El aumento de gasto en los últimos\ntres meses ha sido de ${}".format(dif))
 
 
-#(a,b,c) = recomendacion()
-#print(a)
-#print(b)
-#print(c)
+(a,b,c) = general()
+print(a)
+print(b)
+print(c)
